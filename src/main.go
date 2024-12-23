b60ddab9211e4a2e0c6a5b8782a6c870bc376934
camillepolice@icloud.com <Ap4hgfpaz.>
2024-12-23 21:36:44 +0000

    feat: get folder path with command line

--- src/main.go
+++ src/main.go
@@ -2,25 +2,40 @@ package main
 
 import (
 	"file-organizer/src/services"
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
+	path := flag.String("path", "", "Path to organize files")
+	flag.Parse()
+
+	if *path == "" {
+		fmt.Println("Please provide a path using -path flag")
+		os.Exit(1)
+	}
+
+	if _, err := os.Stat(*path); os.IsNotExist(err) {
+		fmt.Printf("Path %s does not exist\n", *path)
+		os.Exit(1)
+	}
+
 	folderService := services.NewFolderService()
 	fileService := services.NewFileService()
 	organizerService := services.NewOrganizerService(fileService, folderService)
-	path := "../test-folder"
 
-	files, err := folderService.GetAllFiles(path)
-
+	files, err := folderService.GetAllFiles(*path)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error getting files: %v\n", err)
+		os.Exit(1)
 	}
 
 	if len(files) == 0 {
 		fmt.Println("No files found")
+		return
 	}
 
-	folderService.PrepareFolders(path)
-	organizerService.OrganizeFiles(files, path)
+	folderService.PrepareFolders(*path)
+	organizerService.OrganizeFiles(files, *path)
 }
